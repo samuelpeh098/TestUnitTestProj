2be54d82094a035da1fc52a39b2fcef4c5279a83
samuelpeh098 <samuelpeh098@gmail.com>
2019-01-08 15:44:09 +0800

    added NUnit test

---
+++ TestUnitTestProj/UnitTest/HomeControllerTests/HomeControllerTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using NUnit.Framework;
+using TestUnitTestProj.Controllers;
+
+namespace TestUnitTestProj.UnitTest.HomeControllerTests
+{
+    [TestFixture]
+    public class HomeControllerTests
+    {
+        [Test]
+        public void Index_ReturnsActionResult()
+        {
+            var controller = new HomeController();
+            var result = controller.Index();
+            Assert.IsNotNull(result);
+        }
+    }
+};
